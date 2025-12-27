67276e374c8ec0140e60c319cc7ee4b95f4e3c07
spartan-readytocode <7007966033abc@gmail.com>
2025-12-27 10:28:24 +0530

    setup : auth with better auth

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-    out: './drizzle',
+    out: './drizzle/migrations',
     schema: './drizzle/src/db/schemas/schema.ts',
     dialect: 'postgresql',
     dbCredentials: {
--- drizzle/src/index.ts
+++ drizzle/src/index.ts
@@ -1,5 +1,14 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/bun-sql';
-export const db = drizzle(process.env.DATABASE_URL!);
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "./db/schemas/schema"
 
+if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL environment variable is not set");
+}
 
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(pool, { schema });
